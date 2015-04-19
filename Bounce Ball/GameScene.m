ddfe8f8c8d80cffc4b6cbcce63f5b4982c7da72a
Paul <xzhou14@illinois.edu>
2015-04-18 23:16:52 -0500

    ljk

--- Bounce Ball/GameScene.m
+++ Bounce Ball/GameScene.m
@@ -181,13 +181,13 @@ applicationDidBecomeActive = YES;
 - (void)addBall:(CGPoint) location scale:(float) scale{
     // Create ball
     SKSpriteNode * ball = [SKSpriteNode spriteNodeWithImageNamed:@"1"];
-    ball.size = CGSizeMake(self.frame.size.height/20, self.frame.size.height/20);
-    ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.size.width * scale];
+    ball.size = CGSizeMake(self.frame.size.height/20 * scale, self.frame.size.height/20 * scale);
+    ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.size.width * 0.5 * scale];
     ball.physicsBody.dynamic = YES;
     //   ball.physicsBody.collisionBitMask = 1;
     ball.physicsBody.categoryBitMask = ballCategory;
     ball.physicsBody.contactTestBitMask = ballCategory | bottomCategory | fingerCategory | edgeCategory;
-    ball.physicsBody.mass = 0.8;
+    ball.physicsBody.mass = 0.8*scale;
     ball.physicsBody.restitution = 0.5;
     
     // Create the ball slightly off-screen along the right edge,
@@ -257,12 +257,13 @@ applicationDidBecomeActive = YES;
     
     //if both objects are ball and the mode is combined
     if ((firstBody.categoryBitMask & ballCategory) != 0 &&
-        (secondBody.categoryBitMask & ballCategory) != 0) &&
-        (mode == 1))
+        (secondBody.categoryBitMask & ballCategory) != 0 &&
+        (mode == 1) && (firstBody.mass == secondBody.mass))
     {
+        float scale = firstBody.mass/0.8*1.5;
         [secondBody.node removeFromParent];
         [firstBody.node removeFromParent];
-
+        [self addBall:contact.contactPoint scale:scale];
     }
 }
 
@@ -273,14 +274,14 @@ applicationDidBecomeActive = YES;
     CGPoint location = CGPointMake(-1, -1);
     
     if(flag==0){
-        [self addBall:location scale:0.5];
+        [self addBall:location scale:1];
         flag=1;
     }
     
     self.lastSpawnTimeInterval += timeSinceLast;
     if (self.lastSpawnTimeInterval > 5) {
         self.lastSpawnTimeInterval = 0;
-        [self addBall:location scale:0.5];
+        [self addBall:location scale:1];
     }
     
 }
